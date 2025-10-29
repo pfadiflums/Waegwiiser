474814d51bda0aa4682118f4b399b347448c4692
alex.shahini <alex.shahini@kyberna.com>
2025-10-29 16:05:26 +0100

    Stufe Endpoint

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,12 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-      {
-        path: '',
-        loadComponent: () => import('./feature/home/home').then(m => m.Home),
-      },
+  {
+    path: '',
+    loadComponent: () => import('./feature/home/home').then(m => m.Home),
+  },
+  {
+    path: 'stufe/:stufe',
+    loadComponent: () => import('./feature/stufe/stufe').then(m => m.Stufe),
+  },
 ];

e2549125977a4e0f02f9da212ef03b3e5421b300
mitja-kurath <mitja.kurath@mitjakurath.ch>
2026-04-28 15:36:09 +0200

    Added Developer-Tag on Footer

--- src/app/feature/join/join.ts
+++ src/app/feature/join/join.ts
@@ -1,10 +1,9 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-join',
   standalone: true,
-  imports: [RouterLink],
+  imports: [],
   templateUrl: './join.html',
   styleUrl: './join.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,

374e5453124edd371516d168fe28be31837091a7
mitja-kurath <mitja.kurath@mitjakurath.ch>
2026-05-06 14:17:32 +0200

    Temporarily remove admin dashboard

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,6 +4,6 @@ export const environment: Environment = {
   production: true,
   apiUrl: 'https://api.pfadiflums.ch/api/v1',
   version: '1.0.0',
-  oauth2RedirectUrl: 'https://st-justus.ch/oauth2/redirect',
-  midataAuthUrl: 'https://api.st-justus.ch/oauth2/authorization/midata'
+  oauth2RedirectUrl: 'https://api.pfadiflums.ch/oauth2/redirect',
+  midataAuthUrl: 'https://api.pfadiflums.ch/oauth2/authorization/midata'
 };

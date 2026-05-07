769b6a2a98a2fc1fe860fd4ddfc11678f5865638
mitja-kurath <mitja.kurath@mitjakurath.ch>
2026-05-07 13:51:47 +0200

    Fix blob overflow & fix all tailwindcss warnings

--- src/app/guards/role.guard.ts
+++ src/app/guards/role.guard.ts
@@ -2,7 +2,7 @@ import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 import { AuthService } from '../services/auth.service';
 
-export const roleGuard: CanActivateFn = (route, state) => {
+export const roleGuard: CanActivateFn = (route) => {
   const authService = inject(AuthService);
   const router = inject(Router);
 
